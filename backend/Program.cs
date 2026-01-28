367304df19b100e1e294693ff7b490e0f581e4c7
karthisk869511 <karthisk869511@gmail.com>
2026-01-28 19:04:51 +0530

    Unwanted things removed

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,16 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Swagger / OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS so React can call the API
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -25,14 +22,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Auto-create database / tables if they don't exist
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
 
-// Enable Swagger UI in Development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
